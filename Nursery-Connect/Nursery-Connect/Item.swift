ead62dd04945536096cfdf2b20dfbd01657e4512
Ruwanya Hettiarachchi <151648609+ruwanyahettiarachchi@users.noreply.github.com>
2026-03-25 20:06:30 +0530

    Nursery connect project setup and app initialization

---
+++ Nursery-Connect/Nursery-Connect/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Nursery-Connect
+//
+//  Created by ruwanya hettiarachchi on 2026-03-25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
